dd7b6762d33b4471eb1763637ce747aac2d4d926
sahilkruz07 <105875309+sahilkruz07@users.noreply.github.com>
2022-08-04 14:27:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,319 @@
+function playc2()
+{
+    var audio = new Audio('notes/c2.mp3');
+    audio.play();
+}
+function playcs2()
+{
+    var audio = new Audio('notes/db2.mp3');
+    audio.play();
+}
+function playd2()
+{
+    var audio = new Audio('notes/d2.mp3');
+    audio.play();
+}
+function playds2()
+{
+    var audio = new Audio('notes/eb2.mp3');
+    audio.play();
+}
+function playe2()
+{
+    var audio = new Audio('notes/e2.mp3');
+    audio.play();
+}
+function playf2()
+{
+    var audio = new Audio('notes/f2.mp3');
+    audio.play();
+}
+function playfs2()
+{
+    var audio = new Audio('notes/gb2.mp3');
+    audio.play();
+}
+function playg2()
+{
+    var audio = new Audio('notes/g2.mp3');
+    audio.play();
+}
+function playgs2()
+{
+    var audio = new Audio('notes/ab2.mp3');
+    audio.play();
+}
+function playa2()
+{
+    var audio = new Audio('notes/a2.mp3');
+    audio.play();
+}
+function playas2()
+{
+    var audio = new Audio('notes/bb2.mp3');
+    audio.play();
+}
+function playb2()
+{
+    var audio = new Audio('notes/b2.mp3');
+    audio.play();
+}
+
+
+
+/////////////////////////////////////////////////////////
+function playc3()
+{
+    var audio = new Audio('notes/c3.mp3');
+    audio.play();
+}
+function playcs3()
+{
+    var audio = new Audio('notes/db3.mp3');
+    audio.play();
+}
+function playd3()
+{
+    var audio = new Audio('notes/d3.mp3');
+    audio.play();
+}
+function playds3()
+{
+    var audio = new Audio('notes/eb3.mp3');
+    audio.play();
+}
+function playe3()
+{
+    var audio = new Audio('notes/e3.mp3');
+    audio.play();
+}
+function playf3()
+{
+    var audio = new Audio('notes/f3.mp3');
+    audio.play();
+}
+function playfs3()
+{
+    var audio = new Audio('notes/gb3.mp3');
+    audio.play();
+}
+function playg3()
+{
+    var audio = new Audio('notes/g3.mp3');
+    audio.play();
+}
+function playgs3()
+{
+    var audio = new Audio('notes/ab3.mp3');
+    audio.play();
+}
+function playa3()
+{
+    var audio = new Audio('notes/a3.mp3');
+    audio.play();
+}
+function playas3()
+{
+    var audio = new Audio('notes/bb3.mp3');
+    audio.play();
+}
+function playb3()
+{
+    var audio = new Audio('notes/b3.mp3');
+    audio.play();
+}
+
+
+
+/////////////////////////////////////////////////////////////
+function playc4()
+{
+    var audio = new Audio('notes/c4.mp3');
+    audio.play();
+}
+function playcs4()
+{
+    var audio = new Audio('notes/db4.mp3');
+    audio.play();
+}
+function playd4()
+{
+    var audio = new Audio('notes/d4.mp3');
+    audio.play();
+}
+function playds4()
+{
+    var audio = new Audio('notes/eb4.mp3');
+    audio.play();
+}
+function playe4()
+{
+    var audio = new Audio('notes/e4.mp3');
+    audio.play();
+}
+function playf4()
+{
+    var audio = new Audio('notes/f4.mp3');
+    audio.play();
+}
+function playfs4()
+{
+    var audio = new Audio('notes/gb4.mp3');
+    audio.play();
+}
+function playg4()
+{
+    var audio = new Audio('notes/g4.mp3');
+    audio.play();
+}
+function playgs4()
+{
+    var audio = new Audio('notes/ab4.mp3');
+    audio.play();
+}
+function playa4()
+{
+    var audio = new Audio('notes/a4.mp3');
+    audio.play();
+}
+function playas4()
+{
+    var audio = new Audio('notes/bb4.mp3');
+    audio.play();
+}
+function playb4()
+{
+    var audio = new Audio('notes/b4.mp3');
+    audio.play();
+}
+
+
+/////////////////////////////////////////////////////
+
+function playc5()
+{
+    var audio = new Audio('notes/c5.mp3');
+    audio.play();
+}
+function playcs5()
+{
+    var audio = new Audio('notes/db5.mp3');
+    audio.play();
+}
+function playd5()
+{
+    var audio = new Audio('notes/d5.mp3');
+    audio.play();
+}
+function playds5()
+{
+    var audio = new Audio('notes/eb5.mp3');
+    audio.play();
+}
+function playe5()
+{
+    var audio = new Audio('notes/e5.mp3');
+    audio.play();
+}
+function playf5()
+{
+    var audio = new Audio('notes/f5.mp3');
+    audio.play();
+}
+function playfs5()
+{
+    var audio = new Audio('notes/gb5.mp3');
+    audio.play();
+}
+function playg5()
+{
+    var audio = new Audio('notes/g5.mp3');
+    audio.play();
+}
+function playgs5()
+{
+    var audio = new Audio('notes/ab5.mp3');
+    audio.play();
+}
+function playa5()
+{
+    var audio = new Audio('notes/a5.mp3');
+    audio.play();
+}
+function playas5()
+{
+    var audio = new Audio('notes/bb5.mp3');
+    audio.play();
+}
+function playb5()
+{
+    var audio = new Audio('notes/b5.mp3');
+    audio.play();
+}
+
+
+//////////////////////////////////////////////////
+function playc6()
+{
+    var audio = new Audio('notes/c6.mp3');
+    audio.play();
+}
+function playcs6()
+{
+    var audio = new Audio('notes/db6.mp3');
+    audio.play();
+}
+function playd6()
+{
+    var audio = new Audio('notes/d6.mp3');
+    audio.play();
+}
+function playds6()
+{
+    var audio = new Audio('notes/eb6.mp3');
+    audio.play();
+}
+function playe6()
+{
+    var audio = new Audio('notes/e6.mp3');
+    audio.play();
+}
+function playf6()
+{
+    var audio = new Audio('notes/f6.mp3');
+    audio.play();
+}
+function playfs6()
+{
+    var audio = new Audio('notes/gb6.mp3');
+    audio.play();
+}
+function playg6()
+{
+    var audio = new Audio('notes/g6.mp3');
+    audio.play();
+}
+function playgs6()
+{
+    var audio = new Audio('notes/ab6.mp3');
+    audio.play();
+}
+function playa6()
+{
+    var audio = new Audio('notes/a6.mp3');
+    audio.play();
+}
+function playas6()
+{
+    var audio = new Audio('notes/bb6.mp3');
+    audio.play();
+}
+function playb6()
+{
+    var audio = new Audio('notes/b6.mp3');
+    audio.play();
+}
+
+
+////////////////////////////////////////////////////
+
---
+++ main2.js
@@ -0,0 +1,644 @@
+window.addEventListener('keydown', checkPressdown , false);
+window.addEventListener('keyup', checkPressup , false);
+/////////////////////////////////////////////////
+
+function checkPressdown(event){
+
+if(event.key == "q")
+{
+    playc3();
+    var key1 = document.getElementById('c3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "2")
+{
+    playcs3();
+    var key1 = document.getElementById('cs3');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "w")
+{
+    playd3();
+    var key1 = document.getElementById('d3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "3")
+{
+    playds3();
+    var key1 = document.getElementById('ds3');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "e")
+{
+    playe3();
+    var key1 = document.getElementById('e3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "r")
+{
+    playf3();
+    var key1 = document.getElementById('f3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "5")
+{
+    playfs3();
+    var key1 = document.getElementById('fs3');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "t")
+{
+    playg3();
+    var key1 = document.getElementById('g3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "6")
+{
+    playgs3();
+    var key1 = document.getElementById('gs3');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "y")
+{
+    playa3();
+    var key1 = document.getElementById('a3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "7")
+{
+    playas3();
+    var key1 = document.getElementById('as3');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "u")
+{
+    playb3();
+    var key1 = document.getElementById('b3');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+//////////////////////////////////
+if(event.key == "i")
+{
+    playc4();
+    var key1 = document.getElementById('c4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "9")
+{
+    playcs4();
+    var key1 = document.getElementById('cs4');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "o")
+{
+    playd4();
+    var key1 = document.getElementById('d4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "0")
+{
+    playds4();
+    var key1 = document.getElementById('ds4');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "p")
+{
+    playe4();
+    var key1 = document.getElementById('e4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "z")
+{
+    playf4();
+    var key1 = document.getElementById('f4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "s")
+{
+    playfs4();
+    var key1 = document.getElementById('fs4');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "x")
+{
+    playg4();
+    var key1 = document.getElementById('g4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "d")
+{
+    playgs4();
+    var key1 = document.getElementById('gs4');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "c")
+{
+    playa4();
+    var key1 = document.getElementById('a4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "f")
+{
+    playas4();
+    var key1 = document.getElementById('as4');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "v")
+{
+    playb4();
+    var key1 = document.getElementById('b4');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+
+///////////////////////////////////////////////////
+if(event.key == "b")
+{
+    playc5();
+    var key1 = document.getElementById('c5');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "h")
+{
+    playcs5();
+    var key1 = document.getElementById('cs5');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "n")
+{
+    playd5();
+    var key1 = document.getElementById('d5');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "j")
+{
+    playds5();
+    var key1 = document.getElementById('ds5');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "m")
+{
+    playe5();
+    var key1 = document.getElementById('e5');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == ",")
+{
+    playf5();
+    var key1 = document.getElementById('f5');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == "l")
+{
+    playfs5();
+    var key1 = document.getElementById('fs5');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == ".")
+{
+    playg5();
+    var key1 = document.getElementById('g5');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+if(event.key == ";")
+{
+    playgs5();
+    var key1 = document.getElementById('gs5');
+    key1.style.height="135px";
+    key1.style.background = "linear-gradient(to top,black,#0f0f0f,#202020)";
+    key1.style.padding ="110px 0px 0px 0px";
+    key1.style.boxShadow ="3px 3px 10px #202020";
+}
+if(event.key == "/")
+{
+    playa5();
+    var key1 = document.getElementById('a5');
+    key1.style.height="250px";
+    key1.style.background = "linear-gradient(to top,white,#68d8d6,#07beb8)";
+    key1.style.padding ="210px 0px 0px 0px";
+    key1.style.boxShadow ="4px 4px 12px white";
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function checkPressup(event){
+
+if(event.key == "q")
+{
+    var key1 = document.getElementById('c3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+
+if(event.key == "2")
+{
+    var key1 = document.getElementById('cs3');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+
+if(event.key == "w")
+{
+    var key1 = document.getElementById('d3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "3")
+{
+    var key1 = document.getElementById('ds3');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "e")
+{
+    var key1 = document.getElementById('e3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "r")
+{
+    var key1 = document.getElementById('f3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "5")
+{
+    var key1 = document.getElementById('fs3');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "t")
+{
+    var key1 = document.getElementById('g3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "6")
+{
+    var key1 = document.getElementById('gs3');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "y")
+{
+    var key1 = document.getElementById('a3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "7")
+{
+    var key1 = document.getElementById('as3');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "u")
+{
+    var key1 = document.getElementById('b3');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+////////////////////////////////////////////////////
+if(event.key == "i")
+{
+    var key1 = document.getElementById('c4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "9")
+{
+    var key1 = document.getElementById('cs4');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "o")
+{
+    var key1 = document.getElementById('d4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "0")
+{
+    var key1 = document.getElementById('ds4');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "p")
+{
+    var key1 = document.getElementById('e4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "z")
+{
+    var key1 = document.getElementById('f4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "s")
+{
+    var key1 = document.getElementById('fs4');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "x")
+{
+    var key1 = document.getElementById('g4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "d")
+{
+    var key1 = document.getElementById('gs4');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "c")
+{
+    var key1 = document.getElementById('a4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "f")
+{
+    var key1 = document.getElementById('as4');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "v")
+{
+    var key1 = document.getElementById('b4');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+//////////////////////////////////
+if(event.key == "b")
+{
+    var key1 = document.getElementById('c5');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "h")
+{
+    var key1 = document.getElementById('cs5');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+
+if(event.key == "n")
+{
+    var key1 = document.getElementById('d5');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "j")
+{
+    var key1 = document.getElementById('ds5');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "m")
+{
+    var key1 = document.getElementById('e5');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+
+if(event.key == ",")
+{
+    var key1 = document.getElementById('f5');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == "l")
+{
+    var key1 = document.getElementById('fs5');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == ".")
+{
+    var key1 = document.getElementById('g5');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+if(event.key == ";")
+{
+    var key1 = document.getElementById('gs5');
+    key1.style.height="125px";
+    key1.style.padding ="100px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="black";
+    
+}
+if(event.key == "/")
+{
+    var key1 = document.getElementById('a5');
+    key1.style.height="240px";
+    key1.style.padding ="200px 0px 0px 0px";
+    key1.style.boxShadow ="none";
+    key1.style.background="white";
+    
+}
+
+}
